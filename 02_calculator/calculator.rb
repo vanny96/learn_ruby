60ad8b429442b3ffedcb73d1b48a97d8aa3d6408
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-03-08 14:44:45 +0100

    Added solution for third exercise and enabled pedding tests!

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,36 @@
 #write your code here
+def add number1, number2
+    number1 + number2
+end
+
+def subtract (number1, number2)
+    number1 - number2
+end
+
+def sum numbers
+    result = 0
+    numbers.each  do |number|
+        result = result + number
+    end
+    result
+end
+
+def multiply numbers
+    result = 1
+    numbers.each do |number|
+        result = result * number
+    end
+    result
+end
+
+def power number1, number2
+    number1 ** number2
+end
+
+def factorial number
+    result = 1
+    for i in 1..number do
+        result = result * i
+    end
+    result
+end
