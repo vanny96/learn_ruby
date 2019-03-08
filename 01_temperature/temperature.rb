8d9748455dccc204176108e714ac2b53137670a7
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-03-08 14:11:19 +0100

    Solution for second exercise

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
 #write your code here
+def ftoc temperature
+    (temperature - 32) * 5.0/9.0
+end
+def ctof temperature
+    temperature * 9.0/5.0 + 32
+end
+
--- 01_temperature/temperature_spec.rb
+++ 01_temperature/temperature_spec.rb
@@ -54,7 +54,7 @@ describe "temperature conversion functions" do
     end
 
     it "converts body temperature" do
-      expect(ctof(37)).to be_within(0.1).of(98.6)
+      expect(ctof(37)).to be_within(0.1).of(98.6) 
       # Why do we need to use be_within?
       # See http://www.ruby-forum.com/topic/169330
       # and http://en.wikipedia.org/wiki/IEEE_754-2008
