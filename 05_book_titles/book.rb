b793ac1535d77cc12b2af71b175e736599caa37d
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-03-08 16:00:34 +0100

    Added solution to the sixth exercise

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
 class Book
-# write your code here
+    attr_accessor :title
+    def title=(new_title)
+        words = new_title.split
+        words.each do |word|
+            if word == words[0]
+                word.capitalize!
+            elsif word != "the" &&
+                  word != "a" &&
+                  word != "an" &&
+                  word != "and" &&
+                  word != "of" &&
+                  word != "in"
+                word.capitalize!
+            end
+        end
+        @title = words.join(" ")
+    end
 end
